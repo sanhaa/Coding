df9864e7a11a2025c3a51eed3dbb2115ba3e3685
San-Ha Lee <livia3609@gmail.com>
2021-04-07 11:10:15 +0900

    Update J_1840.cpp
    세그먼트 트리 구조체(사실 만들 필요 없긴 함)는 시간 제한 걸리고
    그냥 Tree 배열은 시간 초과 X
    세그먼트 트리는 완전 이진 트리 아닐 수 있지만 빈자리 0으로 하고 (최대 노드 수는 4N)  일차원 배열로 나타내는게 편하다.

--- ps/jungol/J_1840.cpp
+++ ps/jungol/J_1840.cpp
@@ -1,155 +1,55 @@
+// J_1840 구간 최솟값1
+// 세그먼트 트리 구현
 #include <iostream>
-#include <queue>
-
 using namespace std;
 
-const int SIZE = 100;
+const int N_MAX = 50001;
+const int Q_MAX = 200001;
+const int TREE_SIZE = 4 * N_MAX;
 
-//queue <struct pos> q;
-bool board[SIZE][SIZE] = { 0, };
-int M = 0, N = 0;
+int Tree[TREE_SIZE] = { 0, };
+int leaf[N_MAX] = { 0, };
 
-class pos {
-public:
-	int i = 0;
-	int j = 0;
-	int level = 0;
-	pos() {};
-	pos(int _i, int _j, int _level) {
-		i = _i;
-		j = _j;
-		level = _level;
-	}
-};
-
-pos q[100000];
-int front = -1;
-int rear = -1;
-
-
-bool isFull() {
-	if (rear >= SIZE - 1)
-		return true;
-	else
-		return false;
+int max(int a, int b) {
+	return a > b ? a : b;
 }
 
-bool isEmpty() {
-	if (front == rear)
-		return true;
-	else
-		return false;
-}
-
-void enQueue(pos val) {
-	if (isFull())
-		cout << "Queue Overflow \n";
-	else
-		q[++rear] = val;
-}
-
-pos deQueue() {
-	if (isEmpty())
-		cout << "Queue Underflow \n";
+int init(int* a, int index, int start, int end) {
+	if (start == end) return Tree[index] = a[start];
 	else {
-		//return q[++front];
-		pos ret = q[++front];
-		return ret;
+		return Tree[index] = max(init(a, index * 2, start, (start + end) / 2),
+			init(a, index * 2 + 1, (start + end) / 2 + 1, end));
 	}
 }
 
+int query(int index, int start, int end, int left, int right) {
+	if (left > end || right < start) return 0;
+	if (left <= start && right >= end) return Tree[index];
+	//int half = (start + end)/2;
 
-bool check(int i, int j) {
-	if (board[i + 1][j] == 0) return true;
-	if (board[i - 1][j] == 0) return true;
-	if (board[i][j + 1] == 0) return true;
-	if (board[i][j - 1] == 0) return true;
-
-	return false;
+	return max(query(index * 2, start, (start + end) / 2, left, right),
+		query(index * 2 + 1, (start + end) / 2 + 1, end, left, right));
 }
 
-void print() {
-	cout << endl;
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			cout << board[i][j] << " ";
-		}
-		cout << endl;
-	}
-}
 int main() {
-	cin >> N >> M;
-	int count = 0;
-	int hours = 0;
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
 
+	//freopen("input.txt", "r", stdin);
+	int N, Q;
+	cin >> N >> Q;
 
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			cin >> board[i][j];
-		}
+	for (int i = 1; i <= N; i++) {
+		cin >> leaf[i];
 	}
 
+	init(leaf, 1, 1, N);
 
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			if (i == 0 || j == 0 || i == N - 1 || j == N - 1) continue;
-			if ((board[i][j] == 1) && (check(i, j) == true)) {
-				enQueue(pos(i, j, 1));
-				count++;
-			}
-		}
+	for (int i = 0; i < Q; i++) {
+		int left, right;
+		cin >> left >> right;
+		cout << query(1, 1, N, left, right) << "\n";
 	}
-	
-	cout << "count: " << count << endl;
-
-	while (!isEmpty()) {
-
-		pos now = deQueue();
-
-		int i = now.i;
-		int j = now.j;
-		int level = now.level;
-		cout << i << " " << j<<" "<<level << endl;
-
-
-		if (board[now.i][now.j] == false) continue;
-
-		if(check(i, j) == true)
-			board[now.i][now.j] = false; // 치즈가 녹았다
-
-		if (hours < level) {
-			cout <<level<< " ------\n";
-			hours = level;
-			count = 0;
-		}
-		else if (hours == level) {
-			count++;
-		}
-
-		// up
-		if (i - 1 > 1 && board[i - 1][j] == 1) {
-			//enQueue(make_pair(i - 1, j));
-			enQueue(pos(i - 1, j, level+1));
-		}
-		// down
-		if (i + 1 < N && board[i + 1][j] == 1) {
-			//enQueue(make_pair(i + 1, j));
-			enQueue(pos(i + 1, j, level+1));
-		}
-		// left
-		if (j - 1 > 1 && board[i][j-1] == 1) {
-			//enQueue(make_pair(i, j-1));
-			enQueue(pos(i, j-1, level +1));
-		}
-		// right
-		if (j + 1 > 1 && board[i][j+1] == 1) {
-			//enQueue(make_pair(i, j+1));
-			enQueue(pos(i, j+1, level+1));
-		}
-	}
-
-	cout << hours << endl;
-	cout << count << endl;
 
 	return 0;
 }

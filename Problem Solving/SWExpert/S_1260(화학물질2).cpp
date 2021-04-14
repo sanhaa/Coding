fc1746b5ec276ddab6aaab98947262f25c01c7c2
San-Ha Lee <livia3609@gmail.com>
2021-04-14 17:35:28 +0900

    Update S_1260(화학물질2).cpp

--- Problem Solving/SWExpert/S_1260(화학물질2).cpp
+++ Problem Solving/SWExpert/S_1260(화학물질2).cpp
@@ -16,56 +16,52 @@ int main() {
 
 	for (int t = 1; t <= tc; t++) {
 		int N = 0; cin >> N; // <= 100 , 이차원 배열의 가로 세로 길이
-		int M = 0; // matrix 개수
 
 		// N*N 입력
 		int arr[MAX_N][MAX_N] = { 0, };
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
+		for (int i = 0; i < N; i++) 
+			for (int j = 0; j < N; j++) 
 				cin >> arr[i][j];
-			}
-		
-		}
+
 		int Matrix[MAX_M][MAX_M] = { 0, };
 		int d[MAX_M] = { 0, }; // 행렬 개수 
-		pair<int, int> RC[MAX_M];
+		pair<int, int> RC[MAX_M]; // 행렬의 row column 저장
+
 
 		// N*N 에서 행렬 추출하기
 		int row = 0, col = 0;
+		int M = 0; // matrix 개수
+		int ii = 0, jj = 0;
 
 		for (int i = 0; i < N; i++) {
-			col = 0; row = 0;
-
 			for (int j = 0; j < N; j++) {
-				// 0 or 1 skip
-				if (arr[i][j] == 0 || arr[i][j] == -1) continue;
+				col = 0; row = 0;
+				// 0 or -1 skip
+				if (arr[i][j] == 0) continue;
 
-				int ii = i; int jj = j; // 원래의 idx 백업
+				ii = i; jj = j; // 원래의 idx 백업
+
 				while (arr[ii][jj] > 0) {
-					while (arr[ii][jj] > 0) {
-						arr[ii][jj++] = -1; // 이미 확인한 곳임을 표시
-						if(ii == i) col++; 
+					while (arr[ii][jj] > 0) { 
+						arr[ii][jj++] = 0; // 이미 확인한 곳임을 표시
+						if(ii == i) col++;  // 처음 돌 때만 col 세기
 					}
 					jj = j;
 					ii++;  row++;
 				}
-				M++;
-
-				RC[M] = make_pair(row, col);
-				cout << M << " th row col : " << row << " " << col << endl;
-				col = 0; row = 0;
+				RC[++M] = make_pair(row, col);
+				//cout << M << " th row col : " << row << " " << col << endl;
 			}
 		}
 
-		// 행 값 중에 unique 한거 찾기
+		// 행 값 중에 unique 한거 찾기 -> 이게 너무 짜증...
 		for (int i = 1; i <= M; i++) {
 			int unique = 1;
 			for (int j = 1; j <= M; j++) {
 				if (RC[i].first == RC[j].second) unique = 0;
 			}
 			if (unique == 1) {
-				cout << "found " << RC[i].first<<endl;
-				d[0] = RC[i].first;
+				d[0] = RC[i].first; break;
 			}
 		}
 
@@ -76,12 +72,7 @@ int main() {
 					d[i] = RC[j].second; break;
 				}
 			}
-		
 		}
-		for (int i = 1; i <= M; i++) {
-			cout << d[i] << " ";
-		}
-		cout << endl;
 
 		// MCM	
 		for (int r = 2; r <= M; r++) {
@@ -93,13 +84,6 @@ int main() {
 				}
 			} 
 		} 
-
-		for (int i = 1; i <= M; i++) {
-			for (int j = 1; j <= M; j++) {
-				cout << Matrix[i][j] << "  ";
-			}
-			cout << endl;
-		}
 
 		cout << sharp << t << " " << Matrix[1][M] << endl;
 	}

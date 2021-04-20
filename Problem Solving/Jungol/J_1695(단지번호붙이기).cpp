6acfc18afcce77590afa8ff8ac060d0d7c77b2d4
San-Ha Lee <livia3609@gmail.com>
2021-04-20 16:19:16 +0900

    Update and rename J_1695.cpp to J_1695(단지번호붙이기).cpp

--- Problem Solving/Jungol/J_1695(단지번호붙이기).cpp
+++ Problem Solving/Jungol/J_1695(단지번호붙이기).cpp
@@ -1,14 +1,27 @@
 /*
 2021-04-20
 J 1695 단지번호붙이기
-BFS (원래는 flood fill)
+BFS (원래는 flood fill) + queue 구현 + sort 구현
 */
 #include <iostream>
-#include <queue>
-#include <algorithm>
 using namespace std;
 
 #define MAX_N 26
+
+// 오름차순으로 정렬
+int* sort(int* s, int len) {
+	// selection
+	for (int i = 0; i < len; i++) {
+		int min = s[i], mi = i;
+		for (int j = i+1; j < len; j++) {
+			if (min > s[j]) {
+				min = s[j]; mi = j;
+			}
+		}
+		swap(s[mi], s[i]);
+	}
+	return s;
+}
 
 int main() {
 	//freopen("input.txt", "r", stdin);
@@ -26,13 +39,6 @@ int main() {
 		}
 	}
 
-	//for (int i = 0; i < N; i++) {
-	//	for (int j = 0; j < N; j++) {
-	//		cout << map[i][j];
-	//	}
-	//	cout << endl;
-	//}
-
 	// 단지 찾기
 	int G = 0; // 단지 개수
 	int groups[MAX_N] = { 0, }; // groups[G]: 해당 단지 집의 수
@@ -43,36 +49,39 @@ int main() {
 			if (map[i][j] == 0) continue;
 
 			// 집이 있으면
-			queue<pair<int, int>> q;
-			q.push(make_pair(i, j));
+			//queue<pair<int, int>> q;
+			//q.push(make_pair(i, j));
+			pair<int, int> q[500];
+			int front = -1, rear = -1;
+			q[++rear] = make_pair(i, j); // push
 
 			int g = 0; // 이 단지의 집 수
-			while (!q.empty()) {
+			while (/*!q.empty()*/ front != rear) {
 
-				pair<int, int> cur = q.front(); q.pop(); // i j 순서
+				pair<int, int> cur = q[++front]; // pop 
 				if (map[cur.first][cur.second] == 0) continue; // 방문
 				else  map[cur.first][cur.second] = 0;
 
 				g++;
 				// 상
 				if (cur.first - 1 >= 0 && map[cur.first - 1][cur.second] != 0) {
-					q.push(make_pair(cur.first - 1, cur.second));
-					//map[cur.first - 1][cur.second] = 0;
+					//q.push(make_pair(cur.first - 1, cur.second));
+					q[++rear] = make_pair(cur.first-1, cur.second); // push
 				}
 				// 하
 				if (cur.first + 1 < N && map[cur.first + 1][cur.second] != 0) {
-					q.push(make_pair(cur.first + 1, cur.second));
-					//map[cur.first + 1][cur.second] = 0;
+					//q.push(make_pair(cur.first + 1, cur.second));
+					q[++rear] = make_pair(cur.first+1, cur.second); // push
 				}
 				// 좌
 				if (cur.second - 1 >= 0 && map[cur.first][cur.second - 1] != 0) {
-					q.push(make_pair(cur.first, cur.second - 1));
-					//map[cur.first][cur.second - 1] = 0;
+					//q.push(make_pair(cur.first, cur.second - 1));
+					q[++rear] = make_pair(cur.first, cur.second-1); // push
 				}
 				// 우
 				if (cur.second + 1 < N && map[cur.first][cur.second + 1] != 0) {
-					q.push(make_pair(cur.first, cur.second + 1));
-					//map[cur.first][cur.second + 1] = 0;
+					//q.push(make_pair(cur.first, cur.second + 1));
+					q[++rear] = make_pair(cur.first, cur.second+1); // push
 				}
 			}
 
@@ -81,9 +90,11 @@ int main() {
 		}
 	}
 	cout << G << endl;
-	sort(groups, groups + G);
+	sort(groups, G); // my sort function
+
 	for (int i = 0; i < G; i++) {
 		cout << groups[i] << endl;
 	}
+
 	return 0;
 }

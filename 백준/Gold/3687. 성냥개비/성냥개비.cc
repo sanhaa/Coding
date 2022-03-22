81131c323f6e697a1f8192e598679746100f3ae7
San-Ha Lee <livia3609@gmail.com>
2022-03-22 15:46:14 +0900

    [Gold II] Title: 성냥개비, Time: 4 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/3687. 성냥개비/성냥개비.cc
+++ 백준/Gold/3687. 성냥개비/성냥개비.cc
@@ -1,5 +1,6 @@
 // BOJ 3687 성냥개비
 // DP
+// string으로 된 수 비교하기
 #include <iostream>
 #include <string>
 
@@ -9,22 +10,23 @@ const int MAXN = 110;
 int n = 0;
 // req[i] = 숫자 i를 만들기 위해 필요한 성냥개비 개수
 int req[] = { 6, 2, 5, 5, 4, 5, 6, 3, 7, 6 }; 
-/// <summary>
-/// 수의 길이가 최대 50까지라 int 범위를 벗어남
-/// </summary>
+
 string minDP[MAXN] = { "",}; // minDP[i] = i개의 성냥개비로 만들 수 있는 가장 작은 수
 string maxDP[MAXN] = { "",};
 
+/// <summary>
+/// string으로 된 수의 크기 판단
+/// 1. 길이가 길면 무조건 크다.
+/// 2. 길이가 같을 때는 string 부등호로 비교결과 = 정수 비교 결과
+/// </summary>
+/// <param name="a"></param>
+/// <param name="b"></param>
+/// <returns></returns>
 bool compare(string a, string b) {
 	if (a.length() < b.length()) return true;
 	else if (a.length() == b.length()) {
 		int i = 0;
-		while (a[i] == b[i]) {
-			if (i + 1>= a.length()) {
-				return true;
-			}
-			i++;
-		}
+		while (i<a.length()-1 && a[i] == b[i]) {i++;}
 		return a[i] < b[i];
 	}
 	return false;
@@ -58,11 +60,6 @@ int main() {
 				string temp1 = minDP[i - req[num]] + char(num + '0');
 				string temp2 = char(num + '0') + maxDP[i - req[num]];
 				// minv, maxv update
-				/* string으로 된 수의 크기 판단
-					1. minv, maxv가 비어있으면 무조건 업데이트
-					2. 길이가 길면 무조건 크다.
-					3. 길이가 같을 때는 string 부등호로 비교결과 = 정수 비교 결과
-				*/
 				if (temp1[0] != '0') {
 					if (minv == "") minv = temp1;
 					else if (!compare(minv, temp1)) minv = temp1;
@@ -71,7 +68,6 @@ int main() {
 					if (maxv == "")maxv = temp2;
 					else if (compare(maxv, temp2)) maxv = temp2;
 				}
-
 			}
 			minDP[i] = minv;
 			maxDP[i] = maxv;

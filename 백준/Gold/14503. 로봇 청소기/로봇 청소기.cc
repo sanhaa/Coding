b4fb913421491cb8b92f8a41e1315fd318466635
San-Ha Lee <livia3609@gmail.com>
2022-04-29 17:49:01 +0900

    [Gold V] Title: 로봇 청소기, Time: 0 ms, Memory: 2036 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.cc
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.cc
@@ -1,6 +1,7 @@
 // BOJ 14503 로봇 청소기
 // DFS
 #include <iostream>
+#include <stack>
 using namespace std;
 
 struct POS {
@@ -15,10 +16,8 @@ struct POS {
 
 const int MAXN = 60;
 int map[MAXN][MAXN] = { 0, }; // 0: empty, 1: wall
-struct POS st[MAXN * MAXN] ;
-int top = -1;
-int dr[] = { -1, 0, 1, 0 };
-int dc[] = {0, 1, 0, -1};
+stack<struct POS> st;
+int dr[] = { -1, 0, 1, 0 }, dc[] = { 0, 1, 0, -1 };
 
 int left_of(int d) {
 	int nd = d - 1;
@@ -39,34 +38,33 @@ int main() {
 	int ans = 0; // 청소 영역 개수
 	map[r][c] = -1; // 1. clean
 	ans++;
-	st[++top] = POS(r, c, d);
+	st.push(POS(r, c, d));
 
-	while (top>=0) {
-		POS cur = st[top--];
-		
+	while (!st.empty()) {
+		POS cur = st.top(); st.pop();
+
 		// 2. search left
 		int nd = cur.d;
-		int cnt = 0;
-		while (cnt<=4) {
+		int cnt = 0, cleanf = 0; // clean flag
+		for(cnt;cnt<4;cnt++){ // 4번 반복 명시
 			nd = left_of(nd);
 			int nr = cur.r + dr[nd];
 			int nc = cur.c + dc[nd];
 			if (nr < 0 || nc < 0 || nr >= N || nc >= M) continue;
 			if (map[nr][nc] == 0) {
 				map[nr][nc] = -1; // clean
-				ans++;
-				st[++top] = (POS(nr, nc, nd));
+				ans++; cleanf = 1;
+				st.push(POS(nr, nc, nd));
 				break;
 			}
-			cnt++;
 		}
-		if (cnt >= 4) {
+		if (cleanf == 0) {
 			// 네방향 모두 벽 or 청소 = 후진 or 정지
 			int nr = cur.r - dr[cur.d];
 			int nc = cur.c - dc[cur.d];
 			if (nr < 0 || nc < 0 || nr >= N || nc >= M) break;
 			if (map[nr][nc] == 1) break; // 벽: 정지
-			st[++top] = POS(nr, nc, cur.d); // 후진
+			st.push(POS(nr, nc, cur.d)); // else: 후진
 		}
 	}
 	cout << ans << "\n";

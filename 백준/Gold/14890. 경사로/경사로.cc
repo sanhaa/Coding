38563ab70a822ca4ec16ade053a61570bc64300e
San-Ha Lee <livia3609@gmail.com>
2022-04-29 22:41:21 +0900

    [Gold III] Title: 경사로, Time: 0 ms, Memory: 2068 KB -BaekjoonHub

--- 백준/Gold/14890. 경사로/경사로.cc
+++ 백준/Gold/14890. 경사로/경사로.cc
@@ -1,130 +1,107 @@
 // BOJ 14890 경사로
-// 구현
-// i위치 조절 -> cur 위치 조절
 #include <iostream>
-
 using namespace std;
 
 const int MAXN = 110;
-int map[MAXN][MAXN] = { 0, };
 int N = 0, L = 0;
+int board[MAXN][MAXN];
 
-struct POS {
-    int r = 0;
-    int c = 0;
-    POS() {}
-    POS(int _r, int _c) {
-        r = _r; c = _c;
-    }
-};
+bool is_avail(int _r, int _c, int dr, int dc, bool placed[MAXN]) {
+	int r = _r, c = _c;
 
-bool is_road(int r, int c, int dr, int dc) {
-    bool placed[MAXN] = { 0, }; // 경사로 설치 판단, 일차원 매칭 
-    //cout << r << ", " << c << endl;
-    for (int i = 1; i < N; i++) {
-        POS cur = POS(r + dr * i, c + dc * i);
-        POS prev = POS(cur.r - dr, cur.c - dc);
-        //cout<<"diff btw: " << prev.r << " " << prev.c << " ~ " << cur.r << " " << cur.c << endl;
-        int diff = map[prev.r][prev.c] - map[cur.r][cur.c];
-        if (diff == 0) {
-            continue;
-        }
-        else if (diff == 1) { // \ 방향 경사로
-            /////// placed 검사???
-            // 낮은 곳이 L길이만큼 연속인지 판단 (cur부터 앞으로 가며 검사)
-            POS next = POS(cur.r + dr, cur.c + dc);
-            int l = 1;
+	while (true) {
+		int nr = r , nc = c, len = 1;
 
-            while (next.r >= 0 && next.c >= 0 && next.r < N && next.c < N) {
-                if (l >= L) break;
-                if (map[cur.r][cur.c] == map[next.r][next.c]) {
-                    l++;
-                    next.r += dr; next.c += dc;
-                }
-                else break;
-            }
-            if (l < L) {
-                //cout << "L limit\n";
-                return false;
-            }
-            else {
-                // placed cur(i) ~ next(i+l) 까지 설치
-                for (l = 0; l < L; l++) {
-                    //cout << "placed in " << i + l << endl;
-                    placed[i+l] = true;
-                }
-                // prev = next;
-                i += L-1; // cur 위치 조절
-            }
-        }
-        else if (diff == -1) { // / 방향 경사로
-            ////// placed 검사: prev(i-1) ~ prev-l
-            int l = 0;
-            for (l = 0; l < L; l++) {
-                if ((i - 1 - l >= 0) && placed[i - 1 - l]) {
-                    //cout << "already placed \n" << endl;
-                    return false;
-                }
-            }
-            // 낮은 곳이 L 길이만큼 연속인지 판단 (prev부터 뒤로 가며 검사)
-            POS next = POS(prev.r - dr, prev.c - dc);
-            l = 1;
-            while (next.r >= 0 && next.c >= 0 && next.r < N && next.c < N) {
-                   if (l >= L) break;
-                if (map[prev.r][prev.c] == map[next.r][next.c]) {
-                    l++;
-                    next.r -= dr; next.c -= dc;
-                }
-                else break;
-            }
-            if (l < L) {
-                //cout << "L limit\n";
-                return false;
-            }
-            else {
-                for (l = 0; l < L; l++) { 
-                    //cout << "placed in "<<i-1-l<<endl;
-                    // placed next(i-1-l) ~ prev(i-1)까지 설치 
-                    placed[i - 1 - l] = true;
-                }
-                //prev = cur;
-            }
-        }
-        else {
-           // cout << prev.r << " " << prev.c << " | " << cur.r << " " << cur.c << endl;
-            //cout << "height diff >= 2\n";
-            return false;
-        }
-    }
-    return true;
+		while (true) {
+			nr += dr; nc += dc;
+			if (nr < 0 || nc < 0 || nr >= N || nc >= N) break;
+
+			int diff = board[nr][nc] - board[r][c];
+
+			if (diff == 0) {
+				len++;
+				continue;
+			}
+
+			else if (diff == 1) {
+
+				// 1. 길이
+				if (len < L) {
+					return false;
+				}
+
+				for (int l = 1; l <= L; l++) {
+					pair<int, int> target = make_pair(nr - l * dr, nc - l * dc);
+
+					// 2. 범위
+					if (target.first < 0 || target.second < 0 || target.first >= N || target.second >= N) {
+						return false;
+					}
+					// 3. 이미 설치된
+					int pi = 0;
+					if (dr == 0) pi = target.second; // row 검사 = col을 인덱스로 검사
+					else pi = target.first;
+					if (placed[pi]) {
+						return false;
+					}
+				}
+
+				// 사다리 설치 완료
+				// placed 표시
+				for (int l = 1; l <= L; l++) {
+					pair<int, int> target = make_pair(nr - l * dr, nc - l * dc);
+
+					int pi = 0;
+					if (dr == 0) pi = target.second; // row 검사 = col을 인덱스로 검사
+					else pi = target.first;
+					placed[pi] = true;
+				}
+				len = 0; break;
+			}
+			else if (diff >= 2 || diff <= -2) {
+				return false;
+			}
+			else break;
+		}
+		// 사다리 설치 후 r, c 조절 
+		r = nr; c = nc;
+		if (r < 0 || c < 0 || r >= N || c >= N) break;
+	}
+
+	return true;
 }
 
 int main() {
-    cin.tie(NULL);
-    ios_base::sync_with_stdio(false);
+	cin.tie(NULL); ios_base::sync_with_stdio(false);
+	
+	cin >> N >> L;
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++) cin >> board[i][j];
 
-    cin >> N >> L;
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) cin >> map[i][j];
-    }
+	int ans = 0;
 
-    int ans = 0;
-    for (int r = 0; r < N; r++) {
-        int ret = is_road(r, 0, 0, 1);
-        if (ret) {
-            //cout << "true in r: " << r <<endl<< endl;
-            ans++;
-        }
-    }
+	// row 한 줄씩 확인
+	for (int r = 0; r < N; r++) {
+		bool placed[MAXN] = { 0, }; // 경사로 놓은 위치 체크
 
-    for (int c = 0; c < N; c++) {
-        int ret = is_road(0, c, 1, 0);
-        if (ret) {
-            //cout << "true in c: " << c << endl << endl;
-            ans++;
-        }
-    }
+		bool r1 = is_avail(r, 0, 0, 1, placed);
+		bool r2 = is_avail(r, N - 1, 0, -1, placed); // 반대방향
+		if (r1 && r2) {
+			ans++;
+		}
+	}
 
-    cout << ans << "\n";
-    return 0;
+	// col 한 줄씩 확인
+	for (int c = 0; c < N; c++) {
+		bool placed[MAXN] = { 0, };
+
+		bool r1 = is_avail(0, c, 1, 0, placed);
+		bool r2 = is_avail(N-1, c, -1, 0, placed);
+		if (r1 && r2) {
+			ans++;
+		}
+	}
+	cout << ans << "\n";
+
+	return 0;
 }

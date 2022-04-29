2c5349a3890926fa94e9d1fd35066e2c429317d4
San-Ha Lee <livia3609@gmail.com>
2022-04-29 17:14:25 +0900

    [Gold V] Title: 테트로미노, Time: 60 ms, Memory: 10084 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.cc
+++ 백준/Gold/14500. 테트로미노/테트로미노.cc
@@ -1,87 +1,94 @@
-// BOJ 14500 테트로미노
-// 그래프 탐색으로 할려면 ㅗ 모양을 못찾는데...
+// C++ 2차원 배열 회정하기
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
-const int MAXN = 510;
+const int MAXN = 100;
 int N = 0, M = 0;
-int board[MAXN][MAXN] = { 0, };
-bool visited[MAXN][MAXN] = { 0, };
+int maxv = 0;
 
-int dr[] = { -1, 1, 0, 0 };
-int dc[] = { 0, 0, -1, 1 };
-
-int max_sum = 0;
-
-struct POS {
-	int row = 0;
-	int col = 0;
-	int nblocks = 0;
-	int sum = 0;
-	POS() {};
-	POS(int _row, int _col, int _nblocks, int _sum) {
-		row = _row; col = _col; nblocks = _nblocks; sum = _sum;
-	}
+pair<int, int> tetro[7][4] = {
+	{{0, 0}, {0, 1}, {0, 2}, {0, 3}}, // ㅡ
+	{{0, 0}, {1, 0}, {0, 1}, {1, 1}}, // ㅁ
+	{{0, 0}, {1, 0}, {2, 0}, {2, 1}}, // ㄴ
+	{{0, 0}, {1, 0}, {2, 0}, {2, -1}}, // 뒤집은 ㄴ
+	{{0, 0}, {1, 0}, {1, 1}, {2, 1}}, // ㄹ
+	{{0, 0}, {1, 0}, {1, -1}, {2, -1}},// 뒤집은 ㄹ
+	{{0, 0}, {0, 1}, {1, 1}, {0, 2}} // ㅜ
 };
 
-void solution(POS cur) {
-	if (cur.nblocks == 4) {
-		if (max_sum < cur.sum) max_sum = cur.sum;
-		return;
+void print_vector(vector<vector<int>> v) {
+	cout << "--\n";
+	for (int i = 0; i < v.size(); i++) {
+		for (int j=0; j < v[i].size(); j++) {
+			cout << v[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+// from의 row size: from.size()
+// from의 col size: from[0].size()
+vector<vector<int>> rotate_90(vector<vector<int>> from) {
+	int COL = from[0].size(), ROW = from.size();
+	// from의 (col, row) 크기만큼 초기화
+	vector<vector<int>> new_board(COL, vector<int>(ROW, 0)); 
+
+	for (int c = 0; c < COL; c++) {
+		for (int r = 0; r < ROW; r++) {
+			new_board[c][ROW - 1 - r] = from[r][c];
+		}
+	}
+	return new_board;
+}
+
+void solve(vector<vector<int>> board, int depth) {
+	if (depth > 4) return;
+
+	int ROW = board.size(), COL = board[0].size();
+
+	// start point
+	for (int i = 0; i < ROW; i++) {
+		for (int j = 0; j < COL; j++) {
+			// (i, j) 점에서 7가지 모양 확인
+			for (int t = 0; t < 7; t++) {
+				int cur_sum = 0, possible = 1;
+				for (int tt = 0; tt < 4; tt++) { // 각 모양에서 4개의 좌표 정보
+					int ni = i + tetro[t][tt].first, nj = j + tetro[t][tt].second;
+					if (ni < 0 || nj < 0 || ni >= ROW || nj >= COL) {
+						possible = 0; break;
+					}
+					cur_sum += board[ni][nj];
+				}
+				// 이번 모양 
+				if (!possible) continue;
+				if (cur_sum > maxv) maxv = cur_sum;
+			}
+		}
 	}
 
-	for (int dir = 0; dir < 4; dir++) {
-		int nr = cur.row + dr[dir];
-		int nc = cur.col + dc[dir];
-		if (nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
-		if (visited[nr][nc]) continue;
-
-		visited[nr][nc] = true;
-		solution(POS(nr, nc, cur.nblocks + 1, cur.sum + board[nr][nc]));
-		visited[nr][nc] = false;
-	}
+	vector<vector<int>> new_board = rotate_90(board);
+	solve(new_board, depth + 1);
 }
 
 int main() {
 	cin.tie(NULL);
-	cout.tie(NULL);
 	ios_base::sync_with_stdio(false);
 
 	cin >> N >> M;
+	vector<vector<int>> board;
+	board.assign(N, vector<int>(M, 0)); // 2차원 벡터 N*M 초기화
+
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
 			cin >> board[i][j];
 		}
 	}
 
-	// start point
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			// ㅗ 모양
-			POS cur = POS(i, j, 1, board[i][j]);
-			int min = 1000;
-			for (int dir = 0; dir < 4; dir++) {
-				int nr = cur.row + dr[dir];
-				int nc = cur.col + dc[dir];
-				if (nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
-				cur.nblocks++;
-				cur.sum += board[nr][nc];
-				if (min > board[nr][nc]) min = board[nr][nc];
-			}
-			if (cur.nblocks == 4 && max_sum < cur.sum) max_sum = cur.sum;
-			else if (cur.nblocks == 5) {
-				cur.sum -= min; 
-				if (max_sum < cur.sum) max_sum = cur.sum;
-			}
-			// ㅡ ㄴ ㅁ ㄹ 모양은 DFS
-			visited[i][j] = true;
-			solution(POS(i, j, 1, board[i][j]));
-			visited[i][j] = false;
-		}
-	}
-
-	cout << max_sum << endl;
+	// 재귀로 board를 90도씩 회전해서 모든 방향 확인
+	solve(board, 1);
+	cout << maxv << "\n";
 
 	return 0;
 }
